dd31e21ad86f050def8c71a1ffa5680edbd5f9d2
deerowbear@gmail.com <deerowbear@gmail.com>
2024-10-01 12:25:00 -0400

    Add comments

--- file_utils.py
+++ file_utils.py
@@ -3,7 +3,18 @@ from pathlib import Path
 
 class FileUtils:
 
+
     def fileToDictionary(file: str) -> dict:
+        """Creates a dictionary from a file iterating over each line. The 
+        key will be the orginal word and the value will be the sorted value of
+        the word.
+
+        Args:
+            file: Path to the file
+
+        Returns:
+            A dictionary of words
+        """
         dictionary = {}
         with open(file, "r") as file:
             for line in file:
@@ -11,6 +22,17 @@ class FileUtils:
         return dictionary
         
     def validateFile(file: str) -> str:
+        """Validates if a file exists.
+
+        Args:
+            file: Path to the file
+
+        Returns:
+            A path to the file
+
+        Raises:
+            An error if the file is not found.
+        """
         if os.path.isfile(file):
             return file
         else:
--- parsers.py
+++ parsers.py
@@ -3,6 +3,18 @@ import json
 class JsonParser: 
 
     def parseData(json_object: json, key: str) -> str:
+        """Tries to pull a value out of a json object. If the object
+        is of type list it will take the first object in the json and try 
+        and return the value of the key being passed in.
+
+        Args:
+            json_object: A json object that needs a value parsed.
+            key: the key that we expect a value from.
+            
+        Returns:
+            key: a key that will return a value. If no key is found
+            returns None.
+        """
         try:
             if isinstance(json_object, list):
                 return json_object[0][key]
@@ -18,10 +30,22 @@ from file_utils import FileUtils
 class ArgumentParser:
 
     def parseArguments() -> str:
+        """Parses the path argument. If it doesn't exists it will raise an exception.
+        Validation is done on the argument type property.
+
+        Args:
+            None
+
+        Returns:
+            A path to the file
+
+        Raises:
+            An error if no argument is passed in.
+        """
         parser = argparse.ArgumentParser()
         parser.add_argument('--path', type=FileUtils.validateFile, 
                             help='Please define a path to a file.')
-        argument = vars(parser.parse_args());
+        argument = vars(parser.parse_args())
         filePath = JsonParser.parseData(argument, 'path')
         if filePath == None:
             raise Exception("Invalid argument. Please speicify a valid path to a file.")
